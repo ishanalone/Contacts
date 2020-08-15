749d74e59b646d6582131fe8678db01c60639aac
Sushant <sushant.alone@hungama.com>
2020-08-15 23:23:44 +0530

    initial commit

---
+++ Models/user.js
@@ -0,0 +1,30 @@
+'use strict';
+
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id : {
+      type : DataTypes.UUID,
+      primaryKey : true,
+      defaultValue: DataTypes.UUIDV4
+    },
+    email_id : {
+        type : DataTypes.STRING,
+        uniqueKey : true
+    },
+    name : DataTypes.STRING,
+    country_code : DataTypes.INTEGER,
+    created_at: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW()
+    },
+    updated_at: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW()
+    }
+  }, {
+    tableName: 'contact',
+    created_at: 'createdAt', updated_at: 'updatedAt',
+    timestamps: false
+  });
+  return User;
+};
